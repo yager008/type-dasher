c67b16c7f0c00456ee2bc13c5e88145f0cc10469
Yaroslav <yaro.04@mail.ru>
2024-04-21 21:36:01 +0300

    basic controller

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\MainController;
+Route::get('/Hello', [MainController::class, 'index' ])->name('index');
