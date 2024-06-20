9a55926db047290420e72bc2d42e7003faca1ca6
yaro_ <yaro.04@mail.ru>
2024-06-20 14:28:41 +0300

    Api buttons moved to savedTexts tab

--- resources/views/savedTexts.blade.php
+++ resources/views/savedTexts.blade.php
@@ -2,4 +2,22 @@
     @include('type_components.saved_texts')
 
 
+    <form method="POST" action="{{ route('BibleApiController.index') }}">
+        @csrf
+        <label>
+            <button name="BibleButton" id="BibleButton" class="btn btn-primary">
+                RandomBibleVerse
+            </button>
+        </label>
+    </form>
+
+    <form method="POST" action="{{ route('LoremApiController.index') }}">
+        @csrf
+        <label>
+            <button name="LoremButton" id="LoremButton" class="btn btn-primary">
+                LoremButton
+            </button>
+        </label>
+    </form>
+
 </x-app-layout>
