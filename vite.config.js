3f86cbbc0f24e0e4508e9881848d05ad99c2c469
yaro_ <yaro.04@mail.ru>
2024-06-22 13:24:30 +0300

    npm run build finally working

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path'; // Add this line to import the path module
 
 export default defineConfig({
     plugins: [
@@ -8,34 +9,47 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/sass/app.scss',
-
             ],
             refresh: true,
         }),
     ],
+
+    // server: {
+    //     host: '0.0.0.0',
+    //     port: 5173,
+    //     cors: true,
+    //     hmr: {
+    //         host: 'localhost',
+    //         port: 5173,
+    //     },
+    // },
+
     server: {
         host: '0.0.0.0',
-        port: 5173,
-        cors: true,
         hmr: {
+            clientPort: 5173,
             host: 'localhost',
-            port: 5173,
         },
+        watch: {
+            usePolling: true
+        }
     },
+
     build: {
         outDir: 'public/build',
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.js',
-                app_css: 'resources/css/app.css',
-                app_scss: 'resources/sass/app.scss',
-            },
-        },
+        // rollupOptions: {
+        //     input: {
+        //         app: 'resources/js/app.js',
+        //         app_css: 'resources/css/app.css',
+        //         app_scss: 'resources/sass/app.scss',
+        //     },
+        // },
         manifest: true,
     },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'resources/js'),
         },
-    },
+    }
+
 });
